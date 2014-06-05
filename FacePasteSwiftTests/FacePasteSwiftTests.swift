78f51f2eda7a6d3761404df675bf5cdfc4b24079
jackwu <jackwu95@gmail.com>
2014-06-05 12:40:55 -0700

    Initial Commit

---
+++ FacePasteSwiftTests/FacePasteSwiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  FacePasteSwiftTests.swift
+//  FacePasteSwiftTests
+//
+//  Created by Jack Wu on 2014-06-05.
+//  Copyright (c) 2014 Jack Wu. All rights reserved.
+//
+
+import XCTest
+
+class FacePasteSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
